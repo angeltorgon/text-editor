e96b38c4e5b3df128b30384d2025c24a8a004788
Andy Bettisworth <andy@accreu.com>
2020-01-21 16:05:24 -0600

    Implemented mentions using draft-js-mentions plugin.
    Co-authored-by: Angel Torres <alas4224@gmail.com>

--- client/src/components/DocumentEditor.js
+++ client/src/components/DocumentEditor.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import "./styles/document-editor.css"
-import { Editor, EditorState, RichUtils, Modifier } from 'draft-js';
+import { EditorState, RichUtils, Modifier } from 'draft-js';
+import createMentionPlugin, {
+  defaultSuggestionsFilter
+} from 'draft-js-mention-plugin';
+import Editor from 'draft-js-plugins-editor';
+import mentions from './data/mentions'
+import pluginStyles from '../../node_modules/draft-js-mention-plugin/lib/plugin.css';
 
 class DocumentEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      suggestions: mentions
+    };
     this.onChange = editorState => {
       this.setState({ editorState });
     };
     this.onFocus = () => this.refs.editor.focus();
+    this.mentionPlugin = createMentionPlugin();
   }
+
+  onSearchChange = ({ value }) => {
+    this.setState({
+      suggestions: defaultSuggestionsFilter(value, mentions)
+    });
+  };
 
   handleKeyCommand(command, editorState) {
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -24,6 +40,9 @@ class DocumentEditor extends React.Component {
   }
 
   render() {
+    const { MentionSuggestions } = this.mentionPlugin;
+    const plugins = [this.mentionPlugin];
+
     return (
       <div className="editor" onClick={this.onFocus}>
         <Editor
@@ -31,6 +50,12 @@ class DocumentEditor extends React.Component {
           editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
+          plugins={plugins}
+        />
+        <MentionSuggestions
+          onSearchChange={this.onSearchChange}
+          suggestions={this.state.suggestions}
+          onAddMention={this.onAddMention}
         />
       </div>
     );
